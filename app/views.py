cb141f788a48c98e0251a8cfbafb7fbc9b4bd5d8
Derek Onay <wr3tched@gmail.com>
2014-04-10 09:39:32 +0200

    logout function

--- app/views.py
+++ app/views.py
@@ -66,5 +66,8 @@ def after_login(resp):
     login_user(user, remember = remember_me)
     return redirect(request.args.get('next') or url_for('index'))
     
-
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
    
